7ad6d02d64fd6609438c1036c97fa0e677056683
Kurt Peniket <kurt.peniket@gmail.com>
2021-02-19 11:32:50 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  resources :cocktails, only: [:show, ] do
+  resources :cocktails, only: [:show, :index, :new, :create] do
     resources :doses
   end
 end
